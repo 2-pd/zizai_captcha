0f8059a2432d88970f415db371692bca59267b1d
kazuma <>
2023-06-19 16:36:19 +0000

    captcha.phpを実装
    FossilOrigin-Name: e73fac4c9cc064964139a478f025c1ee5fda665ce4b53399ade35358ceefe47e

--- zizai_captcha/captcha.php
+++ zizai_captcha/captcha.php
@@ -1,3 +1,10 @@
 <?php
+include "main.php";
 
-?>
+$zc = new zizai_captcha();
+
+if (!empty($_SERVER["PATH_INFO"])) {
+    $zc->print_image(substr($_SERVER["PATH_INFO"], 1, 10));
+} else {
+    header("HTTP/1.1 404 Not Found");
+}
