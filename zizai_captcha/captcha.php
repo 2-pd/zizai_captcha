33a60e6f4a58ea3178dab636f6eb41ca044f5885
kazuma <>
2023-06-20 08:03:08 +0000

    captcha.phpを実装等
    FossilOrigin-Name: b8f3a60222a0541d159a971785ff2e9385fe0ddb3b67021a23a0a98a5816d7e6

--- zizai_captcha/captcha.php
+++ zizai_captcha/captcha.php
@@ -3,7 +3,7 @@ define("ZIZAI_CAPTCHA_CONFIG_PATH", "config.json");
 
 include "main.php";
 
-$zc = new zizai_captcha();
+$zc = new zizai_captcha(ZIZAI_CAPTCHA_CONFIG_PATH);
 
 if (!empty($_SERVER["PATH_INFO"])) {
     $zc->print_image(substr($_SERVER["PATH_INFO"], 1, 10));
