d9093fa6a6cda959f04cb1b3b3ba95771caa54a4
kazuma <>
2023-06-20 08:58:57 +0000

    zizai_captcha_get_idを追加
    FossilOrigin-Name: b9109acad6aa66311bae2d5287165abaf2cb3c8334d750168fd7501c9eb7ea25

--- zizai_captcha/captcha.js
+++ zizai_captcha/captcha.js
@@ -12,3 +12,20 @@ var zizai_captcha_dir;
     zizai_captcha_dir = path_splitted.join("/");
 }());
 
+function zizai_captcha_get_id (callback_func) {
+    var request_obj = new XMLHttpRequest();
+    
+    request_obj.onreadystatechange = function () {
+        if (request_obj.readyState === 4) {
+            if (request_obj.status === 200) {
+                callback_func(JSON.parse(request_obj.responseText));
+            } else {
+                callback_func(false);
+            }
+        }
+    };
+    
+    request_obj.open("GET", zizai_captcha_dir + ZIZAI_CAPTCHA_GENERATE_ID_ENDPOINT, true);
+    request_obj.timeout = 10000;
+    request_obj.send();
+}
