4bc9e6db0d45e5903e54f402bf89394e871effd8
kazuma <>
2023-06-20 08:34:13 +0000

    captcha.jsの定数設定等
    FossilOrigin-Name: 3c0aca2eaae719997d735d7aa6deb4f9a6b9d201a08f83db863367f3fadd43c6

--- zizai_captcha/captcha.js
+++ zizai_captcha/captcha.js
@@ -1,5 +1,8 @@
 /* Zizai CAPTCHA 23.06-1 */
 
+const ZIZAI_CAPTCHA_GENERATE_ID_ENDPOINT = "generate_id.php";
+const ZIZAI_CAPTCHA_IMAGE_PATH = "image.php";
+
 var zizai_captcha_dir;
 (function(){
     var script_elms = document.getElementsByTagName("script");
