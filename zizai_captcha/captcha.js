81c5e0a112ad66db3e4834585c96ec1cd3ab6c77
kazuma <>
2023-10-11 14:35:36 +0000

    v23.10-2修正版 zizai_captcha_get_htmlで認証用画像のIDを指定可能に
    FossilOrigin-Name: ed57c1e4890fdf247f51e40aa2305de987a6f380b34848b936356bf282d6ce1c

--- zizai_captcha/captcha.js
+++ zizai_captcha/captcha.js
@@ -59,7 +59,7 @@ function zizai_captcha_get_html_callback (data, args) {
     
     var rgb_sum = 0;
     for (var cnt = 0; cnt < 3; cnt++) {
-        rgb_sum += parseInt(args[3].substring(cnt * 2 + 1, cnt * 2 + 3), 16);
+        rgb_sum += parseInt(args[4].substring(cnt * 2 + 1, cnt * 2 + 3), 16);
     }
     
     if (rgb_sum > 382) {
@@ -74,11 +74,11 @@ function zizai_captcha_get_html_callback (data, args) {
     
     args[0](`
 <input type="hidden" name="${args[1]}" id="${args[1]}" value="${data.session_id}">
-<img src="${image_path}" alt="" id="${args[2]}__image" style="vertical-align: text-bottom;"><button type="button" onclick="zizai_captcha_reload_image('${args[2]}__image', '${args[1]}');" style="box-sizing: border-box; width: ${data.image_height}px; height: ${data.image_height}px; background-color: ${args[3]}; background-image: url('${button_image}'); background-size: cover; border: none; margin-left: 5px; vertical-align: text-bottom; cursor: pointer;"></button><br>
+<img src="${image_path}" alt="" id="${args[3]}" style="vertical-align: text-bottom;"><button type="button" onclick="zizai_captcha_reload_image('${args[3]}', '${args[1]}');" style="box-sizing: border-box; width: ${data.image_height}px; height: ${data.image_height}px; background-color: ${args[4]}; background-image: url('${button_image}'); background-size: cover; border: none; margin-left: 5px; vertical-align: text-bottom; cursor: pointer;"></button><br>
 <input type="text" name="${args[2]}" id="${args[2]}" style="box-sizing: border-box; width: ${data.image_width}px; height: ${data.image_height}px; font-size: ${input_font_size}px; letter-spacing: 0.5em; text-align: center;">
 `);
 }
 
-function zizai_captcha_get_html (callback_func, button_color = "#dddddd", id_name = "zizai_captcha_id", characters_name = "zizai_captcha_characters") {
-    zizai_captcha_get_id(zizai_captcha_get_html_callback, [callback_func, id_name, characters_name, button_color]);
+function zizai_captcha_get_html (callback_func, button_color = "#dddddd", id_name = "zizai_captcha_id", characters_name = "zizai_captcha_characters", image_id = "zizai_captcha_image") {
+    zizai_captcha_get_id(zizai_captcha_get_html_callback, [callback_func, id_name, characters_name, image_id, button_color]);
 }
